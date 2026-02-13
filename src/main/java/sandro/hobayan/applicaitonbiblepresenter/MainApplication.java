f5d7cd431986b193f66c348c1ea931ddad60832d
Sandro Hobayan <alesandrohobayan@gmail.com>
2026-02-13 23:16:23 +0800

    feat: tab pane added: bible books, Settings

--- src/main/java/sandro/hobayan/applicaitonbiblepresenter/MainApplication.java
+++ src/main/java/sandro/hobayan/applicaitonbiblepresenter/MainApplication.java
@@ -16,7 +16,6 @@ public class MainApplication extends Application {
         stage.setTitle("Bible Presenter");
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
--- src/main/java/sandro/hobayan/applicaitonbiblepresenter/MainController.java
+++ src/main/java/sandro/hobayan/applicaitonbiblepresenter/MainController.java
@@ -1,14 +1,20 @@
 package sandro.hobayan.applicaitonbiblepresenter;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    private Label lbl;
+    @FXML
+    private TextField book, chapter, verse;
+    @FXML
+    private Button btn;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void btnClick() {
+        lbl.setText("Book: " + book.getText() + "\nChapter: " + chapter.getText() + "\nVerse: " + verse.getText());
     }
 }
